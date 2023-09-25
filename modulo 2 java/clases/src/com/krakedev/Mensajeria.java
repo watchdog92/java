4c0ec83a141d59b08037768b57148f2c3f24c457
watchdog92 <cristhian.pt.1992@gmail.com>
2023-09-24 21:39:36 -0500

    funciones creadas reto 9

---
+++ modulo 2 java/clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+
+public class Mensajeria {
+	public void saludar() {
+		System.out.println("Hola humanos");
+		
+	}
+	public void saludarAmigos(String nombre,String apellido) {
+		System.out.println("Hola"+nombre+" "+apellido);
+	}
+}
